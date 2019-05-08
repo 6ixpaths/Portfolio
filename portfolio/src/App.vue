e268eb07a22057c24a7278a8927c1ccd1c70bc50
Ram Raja <ramraja.vii@gmail.com>
2019-05-08 14:18:04 -0400

    Add About Component
    *Add aboutinfo as data with img, header and info
    *Add CSS
    *Add coding & computer svg's

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Nav/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <About/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Nav from './components/Nav.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Nav
+    Nav,
+    About
   },
   beforeCreate: function() {
       document.body.className = 'home';
---
+++ portfolio/src/components/About.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-11">
+            <div v-for="aboutinfo in aboutinfos" v-bind:key="aboutinfo.id" class="row my-5">
+                <div class="col-lg-4 col-4 text-center">
+                    <img class="img-fluid" v-bind:src="aboutinfo.imgURL" />
+                </div>
+                <div class="col-lg-8 col-8 text-center">
+                    <h2 class=mt-3>{{aboutinfo.header}}</h2>
+                    <p class="text-left mt-3">{{ aboutinfo.info }}</p>
+                </div>
+                <!--
+                <div v-if="isMobile" class="col">
+                    <img class="img-fluid" v-bind:src="aboutinfo.imgURL" />
+                    <h2 class=mt-3>{{aboutinfo.header}}</h2>
+                    <p class="text-left mt-3">{{ aboutinfo.info }}</p>
+                </div>
+                -->
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data(){ 
+        return{
+
+            aboutinfos: [
+                {
+                   header: 'DESIGN WHAT YOU WANT',
+                   info: 'I can create fully responsive and interactive web pages conveying the message that you need',
+                   imgURL: require('../assets/computer.svg')
+                },
+                {
+                   header: 'BUILD WHAT YOU WANT',
+                   info: 'I can create fully responsive and interactive web pages conveying the message that you need',
+                   imgURL: require('../assets/coding.svg')
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+img{
+
+    height: 8vw;
+    object-fit: cover;
+
+}
+
+@media (max-width: 575.98px){
+
+    img{
+
+        height: 35vw;
+
+    }
+
+}
+</style>
+
