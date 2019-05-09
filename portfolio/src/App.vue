89986c838b01e6e76faa3e18c6cb0371db38de68
Ram Raja <ramraja.vii@gmail.com>
2019-05-08 22:32:49 -0400

    Init Contact.vue

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -3,6 +3,7 @@
     <Nav/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <About/>
+    <Contact/>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Nav from './components/Nav.vue'
 import About from './components/About.vue'
+import Contact from './components/Contact.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
     Nav,
-    About
+    About,
+    Contact
   },
   beforeCreate: function() {
       document.body.className = 'home';
---
+++ portfolio/src/components/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='container-fluid'>
+        CONTACT
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+
+<style scoped>
+
+
+@media (max-width: 575.98px){
+
+
+}
+
+</style>
+
+
+
