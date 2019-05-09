1e1069816a5caaf9409f86c1581a4e0197a462b6
Ram Raja <ramraja.vii@gmail.com>
2019-05-08 22:29:12 -0400

    Responsive changes to about.vue
    *Change column width for img & description
    *Add deploy description
    *Add server.svg
    *Change design img

--- portfolio/src/components/About.vue
+++ portfolio/src/components/About.vue
@@ -3,20 +3,14 @@
     <div class="row justify-content-center">
         <div class="col-lg-8 col-11">
             <div v-for="aboutinfo in aboutinfos" v-bind:key="aboutinfo.id" class="row my-5">
-                <div class="col-lg-4 col-4 text-center">
+                <div class="col-lg-4 col-12 text-center">
                     <img class="img-fluid" v-bind:src="aboutinfo.imgURL" />
                 </div>
-                <div class="col-lg-8 col-8 text-center">
+                <div class="col-lg-8 col-12 text-center">
                     <h2 class=mt-3>{{aboutinfo.header}}</h2>
                     <p class="text-left mt-3">{{ aboutinfo.info }}</p>
                 </div>
-                <!--
-                <div v-if="isMobile" class="col">
-                    <img class="img-fluid" v-bind:src="aboutinfo.imgURL" />
-                    <h2 class=mt-3>{{aboutinfo.header}}</h2>
-                    <p class="text-left mt-3">{{ aboutinfo.info }}</p>
-                </div>
-                -->
+
             </div>
         </div>
     </div>
@@ -33,12 +27,17 @@ export default {
                 {
                    header: 'DESIGN WHAT YOU WANT',
                    info: 'I can create fully responsive and interactive web pages conveying the message that you need',
-                   imgURL: require('../assets/computer.svg')
+                   imgURL: require('../assets/graphic-design.svg')
                 },
                 {
                    header: 'BUILD WHAT YOU WANT',
                    info: 'I can create fully responsive and interactive web pages conveying the message that you need',
                    imgURL: require('../assets/coding.svg')
+                },
+                {
+                   header: 'HELP DEPLOY',
+                   info: 'I can create fully responsive and interactive web pages conveying the message that you need',
+                   imgURL: require('../assets/server.svg')
                 }
             ]
         }
@@ -59,7 +58,19 @@ img{
 
     img{
 
-        height: 35vw;
+        height: 50vw;
+
+    }
+
+    h2{
+
+        font-size: 1.5rem;
+
+    }
+
+    p{
+
+        text-align: center !important;
 
     }
 
