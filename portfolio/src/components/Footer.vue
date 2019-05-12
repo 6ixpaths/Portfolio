2f7b08e2eb9be060fc88a26cfae2e64330b039eb
Ram Raja <ramraja.vii@gmail.com>
2019-05-12 03:02:11 -0400

    Wrap footer icons in a tag
    *Add hover with change of background color to social media icons
    *Change col size to 12
    *Wrap icons with anchorage tag

--- portfolio/src/components/Footer.vue
+++ portfolio/src/components/Footer.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
         <div class="row justify-content-center my-2">
-            <div class="col-4">
+            <div class="col">
                 <div class="row justify-content-center">
-                    <div class="col-md-2 col-2 icon-container text-center mx-1 p-0 py-1" v-for="icon in icons" v-bind:key="icon.id">
-                        <img class="img-fluid img-icon" v-bind:src="icon"/>
-                    </div>
+                    <a class="mx-1 p-0 py-1" href="#" v-for="icon in icons" v-bind:key="icon.id">
+                        <div class="col-12 icon-container text-center p-0">
+                            <img class="img-fluid img-icon" v-bind:src="icon"/>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -17,9 +19,7 @@
     </div>
 </template>
 
-
 <script>
-
 
 export default {
     name: 'Footer',
@@ -67,6 +67,12 @@ p{
 
     background-color: #37474F;
     box-shadow: 0 1px 1px 0 rgba(60,64,67,.08),0 1px 3px 1px rgba(60,64,67,.16);
+
+}
+
+.icon-container:hover{
+
+    background-color: #04C2C9;
 
 }
 
