d72fcaf8a241844e4786c48a409ebaa1a4edc891
Ram Raja <ramraja.vii@gmail.com>
2019-05-13 22:02:21 -0400

    Changes App.vue to support scrolling and css changes
    *Add target id's for nav component to scroll to
    *Change formating of contents within template
    *Fix background color error
    *Comment out scrollTo method in case need to be changed for best practice
    *Delete before create as there background color was generated in CSS

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <Nav v-on:scrollTo="scrollTo()"/>
-    <About/>
-    <Skills/>
-    <Contact class="Contact"/>
-    <Footer/>
-  </div>
+    <div id="app">
+        <!--<Nav v-on:scrollTo="scrollTo($event)"/>-->
+        <Nav/>
+        <About id="target1"/>
+        <Skills id="target2"/>
+        <Contact id="target3"/>
+        <Footer id="target4"/>
+    </div>
 </template>
 
 <script>
 
-import Nav from './components/Nav.vue'
-import About from './components/About.vue'
-import Skills from './components/Skills.vue'
-import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
+import Nav from "./components/Nav.vue";
+import About from "./components/About.vue";
+import Skills from "./components/Skills.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Nav,
     About,
@@ -26,50 +27,59 @@ export default {
     Footer
   },
   methods: {
+    /*
+    scrollTo(event){
+      
+     event.preventDefault();
+      console.log("CLICKAZZ");
+      // The id of the section we want to go to.
+      var ele = $("#el1").data("target");
+      var ele2 =  event.target.dataset.target
+      //var ele1 = this.dataset.target.slice(1);
+      //var acele = document.getElementById(ele1); 
+      // An offset to push the content down from the top.
+      var offset = 0;
+      console.log("SELECTED BY JQUERY:" + ele);
+      console.log("SELECTED BY VUE:" + event.target.dataset.target);
+      //console.log(ele1)
+      // Our scroll target : the top position of the
+      // section that has the id referenced by our href.
+      var target = $(ele2).offset().top - offset;
+      //var rect = acele.getBoundingClientRect();
+      //var target1 = (rect.top + document.documentElement.scrollTop) - offset;
 
-    scrollTo(){
+      console.log(target);
+      //console.log(target1);
+  
+      // The magic...smooth scrollin' goodness.
+      $('html, body').animate({scrollTop:target}, 500);
+      //$('#app').innerHtml = "KOTTAI";
 
-      console.log("METHOD RECIEVED FROM CHILD EMIT");
-      window.HTMLElement.prototype.scrollIntoView = function() {};
-      var x= document.getElementsByClassName("Contact");
-      x.scrollIntoView({ behavior: 'smooth'});
-
-    }
-
-  },
-  beforeCreate: function() {
-      document.body.className = 'home';
+      
+    }*/
   }
-  }
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700');
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700");
 
-h2, a, p{
+h2,a,p {
 
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
+  
+}
+
+body{
+
+  background-color: #252934;
 
 }
 
-body.home {
-    background: #252934;;
-}
-
-#app {
-
-  color: #ECEFF1;
-  background: #252934;
-
-}
-
-.header-bar{
-
-    width: 95px;
-    height: 4px;
-    background-color: #FFFFFF;
-
-
+.header-bar {
+  width: 95px;
+  height: 4px;
+  background-color: #ffffff;
 }
 
 </style>
