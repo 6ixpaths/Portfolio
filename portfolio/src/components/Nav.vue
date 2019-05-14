e39acb45bea9d8ea654f51726cea1c8db4aa3078
Ram Raja <ramraja.vii@gmail.com>
2019-05-13 22:04:49 -0400

    Add scroll animation to Nav component
    *Change h1 color as it was deleted from App.vue css
    *Add scroll function
    *Make nav sticky 
    *change background color since it was removed from app.vue

--- portfolio/src/components/Nav.vue
+++ portfolio/src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='container-fluid py-3'>
+    <div class="container-fluid py-3 sticky-top" style="background-color: #252934;">
         <div class="row align-items-center row justify-content-around index-nav-bar">
             <div class="col-xl-8 col-lg-10 col">
                 <nav class="navbar navbar-expand-lg navbar-light">
@@ -10,13 +10,13 @@
                     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul class="nav nav-pills nav-fill flex-column flex-sm-row nav-primary">
                             <li class="nav-item">
-                                <a class="nav-link" v-on:click="scrollTo" href="#">ABOUT</a>
+                                <a class="nav-link" v-on:click="scrollTo($event)" data-target="#target1" href="#">ABOUT</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">SKILLS</a>
+                                <a class="nav-link" v-on:click="scrollTo($event)" data-target="#target2" href="#">SKILLS</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">CONTACT</a>
+                                <a class="nav-link" v-on:click="scrollTo($event)" data-target="#target3" href="#">CONTACT</a>
                             </li>
                         </ul>
                     </div>
@@ -27,19 +27,30 @@
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: 'Nav',
   methods: {
       
-      scrollTo(){
+        scrollTo(event){
 
-          //Another way to pass to parent without child trigger method with v-on
-          //this.$parent.scrollTo();
-          this.$emit('scrollTo');
-          //console.log(this.$root.name);
+            event.preventDefault();
+            // The id of the section we want to go to.
+            var elTarget =  event.target.dataset.target
 
-      }
+            // An offset to push the content down from the top.
+            var offset = 100;
 
+            // Our scroll target : the top position of the
+            // section that has the id referenced by our href.
+            var target = $(elTarget).offset().top - offset;
+            //var rect = acele.getBoundingClientRect();
+            //var target1 = (rect.top + document.documentElement.scrollTop) - offset;
+
+            // The magic...smooth scrollin' goodness.
+            $('html, body').animate({scrollTop:target}, 500);
+
+        }
     }
 }
 </script>
