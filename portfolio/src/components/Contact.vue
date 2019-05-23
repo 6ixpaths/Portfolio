86f0a2d90691e6c7e03e5802c2885763295a1b1b
Ram Raja <ramraja.vii@gmail.com>
2019-05-23 02:47:39 -0400

    http to https

--- portfolio/src/components/Contact.vue
+++ portfolio/src/components/Contact.vue
@@ -81,7 +81,7 @@ export default {
         submit(){
             //headers: {"Access-Control-Allow-Origin": "*"}, //INSERT after first brace
             //User fat arrow to prevent creating local function scope
-            axios.post("http://ramraja.dev/api/sendMail", {
+            axios.post("https://ramraja.dev/api/sendMail", {
                 
                 name: this.name, 
                 email: this.email,
@@ -108,7 +108,7 @@ export default {
 
         submitGet(){
 
-            axios.get("http://ramraja.dev/api/test").then( response => {
+            axios.get("https://ramraja.dev/api/test").then( response => {
 
 
                 console.log(response.data);
